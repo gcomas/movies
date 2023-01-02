3d6c76ebbd491231b737c946266fb24016772af2
Gaboabo <ga26390@hotmail.com>
2023-01-02 18:46:23 -0300

    fix server config

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const jsonServer = require("json-server");
+const clone = require("clone");
+const data = require("./db.json");
+
+const isProductionEnv = process.env.NODE_ENV === "production";
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+
+// For mocking the POST request, POST request won't make any changes to the DB in production environment
+const router = jsonServer.router(isProductionEnv ? clone(data) : "db.json", {
+  _isFake: isProductionEnv,
+});
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-// Add this before server.use(router)
-server.use(
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-  })
-);
+
+server.use((req, _res, next) => {
+  if (req.path !== "/") router.db.setState(clone(data));
+  next();
+});
+
 server.use(router);
-server.listen(3333, () => {
+server.listen(process.env.PORT || 3333, () => {
   console.log("JSON Server is running");
 });
 
