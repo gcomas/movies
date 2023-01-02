1a402e567efbe0e369c0bb0f167854187c3420ef
Gaboabo <ga26390@hotmail.com>
2023-01-02 18:44:25 -0300

    fix server config

--- server.js
+++ server.js
@@ -8,11 +8,10 @@ server.use(middlewares);
 server.use(
   jsonServer.rewriter({
     "/api/*": "/$1",
-    "/blog/:resource/:id/show": "/:resource/:id",
   })
 );
 server.use(router);
-server.listen(3000, () => {
+server.listen(3333, () => {
   console.log("JSON Server is running");
 });
 
